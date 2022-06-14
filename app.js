bc4b1c11b31d91c3aa7a9a51579c46370ad9455a
cip14 <tiagocorx@gmail.com>
2022-06-13 22:45:01 -0300

    better error handling

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ app.use(express.json());
 app.route('/api/orders')
     .get((req, res) => {
         fs.readFile('./json/orders.json', (err, data) => {
-            if (err) return console.log(err)
+            if (err) {
+                res.status(500).send()
+                return console.log(err)
+            }
             res.status(200).send(JSON.parse(data))
         })
     })
@@ -16,9 +19,12 @@ app.route('/api/orders/:id')
     .get((req, res) => {
         const { id } = req.params;
         fs.readFile('./json/orders.json', (err, data) => {
-            if (err) return console.log(err)
+            if (err) {
+                res.status(500).send()
+                return console.log(err)
+            }
             const order = JSON.parse(data).find(e => e.id === parseInt(id, 10))
-            if(order === undefined) return res.status(404).send()
+            if (order === undefined) return res.status(404).send()
             res.status(200).send(order)
         })
     })
@@ -26,7 +32,10 @@ app.route('/api/orders/:id')
 app.route('/api/pizzas')
     .get((req, res) => {
         fs.readFile('./json/pizzas.json', (err, data) => {
-            if (err) return console.log(err);
+            if (err) {
+                res.status(500).send()
+                return console.log(err)
+            }
             res.status(200).send(JSON.parse(data))
         })
     })
