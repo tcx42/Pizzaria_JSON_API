1d13ad2038fcea934ba1dd4ef031ea0666f58e13
cip14 <tiagocorx@gmail.com>
2022-06-23 14:49:40 -0300

    added sqlite

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,60 +1,57 @@
 const request = require('supertest')
-const fs = require('fs')
+const fs = require('fs/promises')
 const api = require('../api')
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database(':memory:', sqlite3.OPEN_READWRITE, (err) => {
-    if (err) return console.log(err);
-})
-beforeAll(() => {
-    db.run(`CREATE TABLE pizzas(id, name, price, ingredients)`, (err) => {
-        if (err) console.log(err)
-        db.run('INSERT INTO pizzas (id, name, price,  ingredients) VALUES (1, "Margherita", 5, "tomato, mozarella")', (err) => {
-            if (err) console.log(err)
-        })
-        db.run('INSERT INTO pizzas (id, name, price,  ingredients) VALUES (2, "Bufala", 6, "tomato, mozarella di bufala")', (err) => {
-            if (err) console.log(err)
-        })
-        db.each('SELECT * FROM pizzas', (err, row) => {
-            if (err) return console.log(err)
-            console.log(row)
-        })
-    })
-    db.run(`CREATE TABLE orders(id, items)`, (err) => {
-        if (err) console.log(err)
-        db.run('INSERT INTO orders (id, items) VALUES (1, "Bufala , 1, Diavola , 2")', (err) => {
-            if (err) console.log(err)
-        })
-        db.each('SELECT * FROM orders', (err, row) => {
-            if (err) return console.log(err)
-            console.log(row)
-        })
-    })
-    db.run('CREATE TABLE items (itemId, orderId, pizza, qtd, PRIMARY KEY (itemId), FOREIGN KEY (orderId) REFERENCES orders())', err => {
-        if (err) console.log(err)
-    })
+const db = new sqlite3.Database(':memory:', sqlite3.OPEN_READWRITE, err => { if (err) return console.error(err) })
+
+beforeAll(async () => {
+    try {
+        const sql = (await fs.readFile('./tests/testData.sql')).toString()
+        db.exec(sql, err => { if (err) return console.error(err) })
+    } catch (error) {
+        console.error(error)
+    }
 })
 afterAll(() => {
-    db.close(err => {
-        if (err) return console.log(err)
-    })
+    db.close(err => { if (err) return console.error(err) });
 })
 
 describe('GET /orders', () => {
     test('should respond with a list of orders', async () => {
         const res = await request(api).get('/api/orders')
-        expect(res.statusCode).toBe(200)
-        fs.readFile('./json/orders.json', (err, data) => {
+        db.all(`SELECT orders.id, pizzas.name, items.quantity FROM orders
+        JOIN items on orders.id = items.order_id
+        JOIN pizzas on items.pizza = pizzas.id ORDER BY orders.id ASC`, (err, rows) => {
+            if (err) return console.error(err);
+            let orders = Array.from(new Set(rows.map((r) => {
+                return r.id;
+            })));
+            for (let i = 0, j = 0; i < rows.length; i++) {
+                if (orders[j] == rows[i].id) {
+                    orders[j] = ({ 'id': rows[i].id, 'items': [{ 'pizza': rows[i].name, 'quantity': rows[i].quantity }] })
+                    j++;
+                } else {
+                    orders[j - 1].items.push({ 'pizza': rows[i].name, 'quantity': rows[i].quantity })
+                }
+            }
+            expect(res.statusCode).toBe(200)
             expect(res.body).toBeDefined
-            expect(data).toBeDefined
-            expect(res.body).toEqual(JSON.parse(data))
+            expect(orders).toBeDefined
+            expect(res.body).toEqual(orders)
         })
     })
 })
 describe('GET /orders/:id', () => {
     test('should respond with details of an order', async () => {
         const res = await request(api).get('/api/orders/1')
-        fs.readFile('./json/orders.json', (err, data) => {
-            expect(res.body).toEqual(JSON.parse(data)[0])
+        db.all(`SELECT orders.id, pizzas.name, items.quantity FROM orders
+        JOIN items on orders.id = items.order_id
+        JOIN pizzas on items.pizza = pizzas.id WHERE orders.id = 1`, (err, rows) => {
+            let order = { 'id': rows[0].id, 'items': [] };
+            for (let i = 0; i < rows.length; i++) {
+                order.items.push({ 'pizza': rows[i].name, 'quantity': rows[i].quantity })
+            }
+            expect(res.body).toEqual(order)
         })
     })
     test('Should respond with 404 case invalid id', async () => {
@@ -66,44 +63,27 @@ describe('GET /pizzas', () => {
     test('should respond with a list of pizzas', async () => {
         const res = await request(api).get('/api/pizzas')
         expect(res.statusCode).toBe(200)
-        fs.readFile('./json/pizzas.json', (err, data) => {
+        db.all(`SELECT pizzas.id, pizzas.name, pizzas.price, ingredients.ingredient FROM pizzas
+        JOIN pizza_ingredients on pizzas.id = pizza_ingredients.pizza_id 
+        JOIN ingredients on pizza_ingredients.ingredient_id = ingredients.id
+        ORDER BY pizzas.id ASC;`, (err, rows) => {
+            if (err) return console.error(err);
+            let pizzas = new Array();
+            let i = 0;
+            let currentPizzaIndex = 0;
+            while (i < rows.length) {
+                pizzas.push({ 'name': rows[i].name, 'price': rows[i].price, 'ingredients': [] })
+                let currentPizza = rows[i].id;
+                while (currentPizza === rows[i].id) {
+                    pizzas[currentPizzaIndex].ingredients.push(rows[i].ingredient);
+                    i++;
+                    if (i >= rows.length) break;
+                }
+                currentPizzaIndex++;
+            }
             expect(res.body).toBeDefined
-            expect(data).toBeDefined
-            expect(res.body).toEqual(JSON.parse(data))
+            expect(pizzas).toBeDefined
+            expect(res.body).toEqual(pizzas)
         })
     })
 })
-// describe('GET /orders', () => {
-//     test('should respond with a list of orders', async () => {
-//         const res = await request(api).get('/api/orders')
-//         expect(res.statusCode).toBe(200)
-//         fs.readFile('./json/orders.json', (err, data) => {
-//             expect(res.body).toBeDefined
-//             expect(data).toBeDefined
-//             expect(res.body).toEqual(JSON.parse(data))
-//         })
-//     })
-// })
-// describe('GET /orders/:id', () => {
-//     test('should respond with details of an order', async () => {
-//         const res = await request(api).get('/api/orders/1')
-//         fs.readFile('./json/orders.json', (err, data) => {
-//             expect(res.body).toEqual(JSON.parse(data)[0])
-//         })
-//     })
-//     test('Should respond with 404 case invalid id', async()=>{
-//         const res = await request(api).get('/api/orders/7')
-//         expect(res.statusCode).toBe(404)
-//     })
-// })
-// describe('GET /pizzas', () => {
-//     test('should respond with a list of pizzas', async () => {
-//         const res = await request(api).get('/api/pizzas')
-//         expect(res.statusCode).toBe(200)
-//         fs.readFile('./json/pizzas.json', (err, data) => {
-//             expect(res.body).toBeDefined
-//             expect(data).toBeDefined
-//             expect(res.body).toEqual(JSON.parse(data))
-//         })
-//     })
-// })
